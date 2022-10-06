774b5a42d19f8d3090f36a51ecb5ad2449de9f44
rafiyandi <rafiyandi1905@email.com>
2022-10-06 14:23:34 +0700

    add clasification model

---
+++ lib/models/klasifikasi_model.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+
+class KlasifikasiModel {
+  String? hasil;
+  String? urlAsli;
+  String? urlGRY;
+  String? urlLBP;
+
+  KlasifikasiModel({this.hasil, this.urlAsli, this.urlGRY, this.urlLBP});
+
+  Map<String, dynamic> toJson() {
+    return {
+      'hasil': hasil,
+      'urlAsli': urlAsli,
+      'urlGRY': urlGRY,
+      'urlLBP': urlLBP
+    };
+  }
+
+  KlasifikasiModel.fromJson(Map<String, dynamic> json) {
+    hasil = json['hasil'];
+    urlAsli = json['urlAsli'];
+    urlGRY = json['urlGRY'];
+    urlLBP = json['urlLBP'];
+  }
+}
